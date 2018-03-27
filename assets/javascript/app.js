3e03cb37c002b801c1a73e2ac1c33dc98f3ff0d4
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-03-26 23:52:44 -0400

    countdown display working -- submit button and scoring still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-// // Function to start game , load questiobs with start button- timer starts, counters reset to zero
-
-// $( document ).ready(function() {
-
 
 
 // Define variables: object for questions. answers, correctanswer, variables (counters) for correct and incorrect guesses
@@ -48,8 +44,15 @@ var correctCount = 0;
 var incorrectCount = 0;
 var unAnsweredCount = 0;
 var checkedButtons = [];
-
+var number=30;
+var newButton;
 var create; 
+var intervalId;
+
+
+$( document ).ready(function() {
+
+    
 
 // //  Start on click of "Start Quiz" button. Hide start quiz button, create new submit answers button and call function to display quiz content.
 $("#startquiz").on("click", function() {
@@ -57,74 +60,83 @@ $("#startquiz").on("click", function() {
       event.preventDefault();
       $("#startquiz").hide();
       $("#score").empty();
-      var correctCount = 0;
+     
+
+        var correctCount = 0;
         var incorrectCount = 0;
         var unAnsweredCount = 0;
 
-      
-
         displayQuizContent();
 
-        // $("#submitAnswers").on("click", function() {
-  
-        //     event.preventDefault();
-            
-      
-        //       scoreCheck();
-
-
-  //  Set the button alert's timeout to run three seconds after the function's called. When time expires, clear the contents of the divs,  
-//   display game over message and call score check funciton for results
-  
-// var countdown;
-// var countdown_number;
-
-// function countdown_init() {
-//     countdown_number = 11;
-//     countdown_trigger();
-// }
-
-// function countdown_trigger() {
-//     if(countdown_number > 0) {
-//         countdown_number--;
-//         $('countdown_text').innerHTML = countdown_number;
-//         if(countdown_number > 0) {
-//             countdown = setTimeout('countdown_trigger()', 1000);
-
-
-//                 $("#quiz-content").empty();
-//                 $("#question").empty();
-//                 $("#submitAnswers").hide();
-//                 $("#score").html("Game Over!<br>");
-            
-//             scoreCheck();     
-
-// 	}
-
-// 	function countdown_clear() {
-// 		clearTimeout(countdown);
-// 	}
-
-    timeUp = setTimeout(function() {
-                
-                    $("#quiz-content").empty();
-                    $("#question").empty();
-                    $("#submitAnswers").hide();
-                    $("#score").html("Game Over!<br>");
-                
-                scoreCheck();
-            
-            
-            }, 20000);
-
+                    run();
         });
+
+//Countdown function
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  //  The decrement function.
+  function decrement() {
+
+    number--;
+
+         //  Dispplay time remaining.
+
+                 $("#status").text("Time Remaining: " +number+ " seconds");
+
+//Call the stop function when timer hits 0, clear out the quiz contents and display score info
+    if (number === 0) {
+        stop();
+
+        function stop() {
+
+            clearInterval(intervalId);
+            number = 30;
+            
+                         
+            $("#quiz-content").empty();
+            $("#status").empty();
+            $("#question").empty();
+            $("#submitAnswers").hide();
+            $("#score").html("Game Over!<br>");
+            $("#score").append("<br>"+"Score: "+correctCount/quizContent.length+"%"+"<br>");
+            $("#score").append("<br>"+"Correct Answers: " + correctCount + "<br>");
+            $("#score").append("<br>"+"Incorrect Answers: " + incorrectCount + "<br>");
+            $("#score").append("<br>"+"Unanswered Questions: " + unAnsweredCount);
+
+            $("#startquiz").show();  
+          }     
+    }
+   
+}
+
+//function to create a Submit button for the radio button form
+  function createSubmit() {
+
+    var newButton = $("<button/>");
+    
+       $(newButton).attr("class","btn btn-info btn-lg center-block");
+       $(newButton).attr("type","Submit");
+       $(newButton).attr("id","submitAnswers");
+       $(newButton).text("Submit Answers");
+       $("#question").append(newButton); 
+       $('#submitAnswers').click(function() {
+        event.preventDefault();
+        stop();
+    });
+
+       
+  }
+
         
 
-//Function to display the questions and choices (with radio buttons)
+//Function to display the questions and choices (with radio buttons), creates new divs/forms for the radio buttons
     
       function displayQuizContent ()  {
 
-        $("#quiz-content").text("Time Remaining: " +30+ " seconds");
+        // $("#quiz-content").text("Time Remaining: " +number+ " seconds");
 
 
           for (var i = 0; i < quizContent.length; i++) {
@@ -163,75 +175,45 @@ $("#startquiz").on("click", function() {
                                              console.log("value: " + value);                                                                              
                         }  
                         
-                     
-                        
-                //        
-                //  }    
-            };
-                 
-            var newButton = $("<button/>");
-            $(newButton).attr("class","btn btn-info btn-lg center-block");
-            $(newButton).attr("type","Submit");
-            $(newButton).attr("id","submitAnswers");
-            $(newButton).text("Submit Answers");
-            $("#question").append(newButton);  
-
-
-
-            $("#submitAnswers").click(function(scoreCheck) {
-
-                event.preventDefault();
-    
-              })
-
-
+            }
+                                createSubmit();
+                                     
 }
 
+//Function to loop through the forms of radio buttons and compare answers that are checked to the correcrt answer - not working!!!
+
+//       scoreCheck();
+
+// scoreCheck();
+//             function scoreCheck()      {
 
 
-        //   $('#submitAnswers').click(function() {
-        //     var selValue = $('input[name=optradio]:checked').val(); 
-        //     $('#score').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
-        // });
+                // for (var k = 0; k < quizContent.length; k++) {
 
+
+                //     var userAnswer = ('#choiceForm'+k+"input[name=optradio]:checked") == true.valueOf();
+
+                //     (checkedButtons).push(userAnswer)
+
+                //     if (checkedButtons[k]===quizContent[k].correctAnswer){
+
+                //         correctCount++
+
+                        // console.log(correctCount)
+                        // console.log(userAnswer)
+                        // console.log("Question" +k+ checkedButtons)
+
+        
+                    
+ 
+     
+               
+    //    }
+      
      
 
 
-
-          function scoreCheck()      {
-
-
-            for (var k = 0; k < quizContent.length; k++) {
-
-
-                var userAnswer = ('#choiceForm'+k+"input[name=optradio]:checked") == true.valueOf();
-
-                (checkedButtons).push(userAnswer)
-
-                if (checkedButtons[k]===quizContent[k].correctAnswer){
-
-                    correctCount++
-
-                        console.log(correctCount)
-                        console.log(userAnswer)
-                        console.log("Question" +k+ checkedButtons)
-
-        
-                    }
-
-                }  
-            $("#score").append("<br>"+"Score: "+correctCount/quizContent.length+"%"+"<br>");
-            $("#score").append("<br>"+"Correct Answers: " + correctCount + "<br>");
-            $("#score").append("<br>"+"Incorrect Answers: " + incorrectCount + "<br>");
-            $("#score").append("<br>"+"Unanswered Questions: " + unAnsweredCount);
-
-            $("#startquiz").show();  
-               
-    }
-                
-        
-
-        // });
+        });
 
             
                           
