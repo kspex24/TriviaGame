433462e40a3d219aabc89f7a90ddbe7ef399c2f1
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-05-30 16:24:59 -0400

    scoring fixes

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Project Description:
 This is a hangman game created using JavaScript, HTML and CSS/Twitter Bootstrap. It tests the user's tv show knowledge.
 
 How does it work? 
-The user has 30 seconds to complete the multiple choice quiz. The questions, amnswers, and correct answer are stored in an object.  When the user clicks Start Quiz, the multiple choice questions and answers with radio buttons will be displayed on the screen.  The questions, answers, and correct answers are stored in an object.
-The user has 30 seconds to complete the quiz. Time remaining will be displayed on the screen.  They can submit their answers bny clicking a submit button or the program will timeout and go to the results screen at the 30 second mark. 
+The user has 30 seconds to complete the multiple choice quiz. The questions, answers, and correct answer are stored in an object.  When the user clicks Start Quiz, the multiple choice questions and answers with radio buttons will be displayed on the screen.  The questions, answers, and correct answers are stored in an object.
+The user has 30 seconds to complete the quiz. Time remaining will be displayed on the screen.  They can submit their answers by clicking a submit button or the program will timeout and go to the results screen at the 30 second mark. 
 
-Once submit is clicked or time expiores, the program will compare the user choices with the correct answers and teh following results will be displayed ont eh results screen:
+Once submit is clicked or time expires, the program will compare the user choices with the correct answers and the following results will be displayed on the results screen:
 
 # Correct Answers:
 # Incorrect Answerss:
@@ -22,4 +22,4 @@ This project is a homework assignment for the second week of Coding Bootcamp for
 
 What is the goal of this project?
 
-The goal of this project is to create a multiple choice quiz with radio buttons, store the user responses and calculate results, and to use the set time out function and display time remaining.
+The goal of this project is to create a multiple choice quiz with radio buttons which is dynamically displayed on the screen, store the user responses and calculate results, and to use the set time out function and display time remaining.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,222 +1,266 @@
+$(document).ready(function() {
+  // Define variables: object for questions. answers, correctanswer, variables (counters) for correct and incorrect guesses
 
-
-// Define variables: object for questions. answers, correctanswer, variables (counters) for correct and incorrect guesses
-
-var quizContent = [
-  {
+  var quizContent = [
+    {
       question: "Which soap opera included the character Erica Kane?",
-      choices: ["All My Children", "One Life to Live", "General Hospital", "The Young & the Restless"], 
+      choices: [
+        "All My Children",
+        "One Life to Live",
+        "General Hospital",
+        "The Young & the Restless"
+      ],
       correctAnswer: 0
-  },
-  {
+    },
+    {
       question: "Which character does Mark Harmon play on NCIS?",
-      choices: ["Anthony Dinozo","Jethro Gibbs", "Timothy McGee ", "Leon Vance"],
+      choices: [
+        "Anthony Dinozo",
+        "Jethro Gibbs",
+        "Timothy McGee ",
+        "Leon Vance"
+      ],
       correctAnswer: 1
-  } ,
-  {
+    },
+    {
       question: "In which series did George Clooney play Dr. Doug Ross?",
-      choices: ["Grey's Anatomy","ER", "Scrubs", "The Good Doctor"],
+      choices: ["Grey's Anatomy", "ER", "Scrubs", "The Good Doctor"],
       correctAnswer: 1
-  },
-  {
+    },
+    {
       question: "What actress played the character Marissa Cooper on The OC?",
-      choices: ["Rachel Bilson", "Mischa Barton", "Kelly Rowan", "Melinda Clarke"],
+      choices: [
+        "Rachel Bilson",
+        "Mischa Barton",
+        "Kelly Rowan",
+        "Melinda Clarke"
+      ],
       correctAnswer: 1
-  },
-  {
+    },
+    {
       question: "Which bingeworthy series included the character Walter White?",
       choices: ["Better Call Saul", "House of Cards", "Narcos", "Breaking Bad"],
       correctAnswer: 3
-  },
-  {
+    },
+    {
       question: "Which show is the longest running on tv with 29 seasons?",
       choices: ["Mash", "Brady Bunch", "Law and Order", "The Simpsons"],
       correctAnswer: 3
-  }   
-];
+    }
+  ];
 
-console.log(quizContent)
+  //Define scoring variables (counters/timer)
 
+  var correctCount;
+  var incorrectCount;
+  var unAnsweredCount;
+  var correctAnswers = [0, 1, 1, 1, 3, 3];
+  var checkedButtons;
+  var number = 30;
+  var create;
+  var intervalId;
 
-//Define scoring variables (counters/timer)
-
-var correctCount = 0;
-var incorrectCount = 0;
-var unAnsweredCount = 0;
-var checkedButtons = [];
-var number=30;
-var newButton;
-var create; 
-var intervalId;
-
-
-$( document ).ready(function() {
-
-    
-
-// //  Start on click of "Start Quiz" button. Hide start quiz button, create new submit answers button and call function to display quiz content.
-$("#startquiz").on("click", function() {
-  
-      event.preventDefault();
-      $("#startquiz").hide();
-      $("#score").empty();
-     
-
-        var correctCount = 0;
-        var incorrectCount = 0;
-        var unAnsweredCount = 0;
-
-        displayQuizContent();
-
-                    run();
-        });
-
-//Countdown function
-function run() {
+  //Countdown function
+  function run() {
+    var number = 30;
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
   }
 
   //  The decrement function.
   function decrement() {
-
     number--;
 
-         //  Dispplay time remaining.
+    //  Display time remaining.
 
-                 $("#status").text("Time Remaining: " +number+ " seconds");
+    $("#status").text("Time Remaining: " + number + " seconds");
 
-//Call the stop function when timer hits 0, clear out the quiz contents and display score info
+    //Call the stop function when timer hits 0, clear out the quiz contents and display score info
     if (number === 0) {
-        stop();
+      stop();
 
-        function stop() {
+      function stop() {
+        clearInterval(intervalId);
+        number = 30;
 
-            clearInterval(intervalId);
-            number = 30;
-            
-                         
-            $("#quiz-content").empty();
-            $("#status").empty();
-            $("#question").empty();
-            $("#submitAnswers").hide();
-            $("#score").html("Game Over!<br>");
-            $("#score").append("<br>"+"Score: "+correctCount/quizContent.length+"%"+"<br>");
-            $("#score").append("<br>"+"Correct Answers: " + correctCount + "<br>");
-            $("#score").append("<br>"+"Incorrect Answers: " + incorrectCount + "<br>");
-            $("#score").append("<br>"+"Unanswered Questions: " + unAnsweredCount);
+        var checkedButtons = [];
+        var correctCount = 0;
+        var incorrectCount = 0;
+        var unAnsweredCount = 0;
+        //Store radio button values in an array
+        var responseVal = $("#choiceForm0 input:checked").val();
+        checkedButtons.push(responseVal);
+        var responseVal = $("#choiceForm1 input:checked").val();
+        checkedButtons.push(responseVal);
+        var responseVal = $("#choiceForm2 input:checked").val();
+        checkedButtons.push(responseVal);
+        var responseVal = $("#choiceForm3 input:checked").val();
+        checkedButtons.push(responseVal);
+        var responseVal = $("#choiceForm4 input:checked").val();
+        checkedButtons.push(responseVal);
+        var responseVal = $("#choiceForm5 input:checked").val();
+        checkedButtons.push(responseVal);
 
-            $("#startquiz").show();  
-          }     
+        console.log("Answers selected: " + checkedButtons);
+        console.log("Correct Answers: " + correctAnswers);
+//compare checked answers to correct answers
+        for (var k = 0; k < 6; k++) {
+          if (checkedButtons[k] === undefined) {
+            unAnsweredCount++;
+          } else if (checkedButtons[k] == correctAnswers[k]) {
+            correctCount++;
+          } else {
+            incorrectCount++;
+          }
+        }
+        //Empty question div and display scoring info
+
+        $("#quiz-content").empty();
+        $("#status").empty();
+        $("#question").empty();
+        $("#submitAnswers").hide();
+        $("#score").html("Game Over!<br>");
+        $("#score").append(
+          "<br>" +
+            "Score: " +
+            Math.round(correctCount / quizContent.length * 100) +
+            "%" +
+            "<br>"
+        );
+        $("#score").append(
+          "<br>" + "Correct Answers: " + correctCount + "<br>"
+        );
+        $("#score").append(
+          "<br>" + "Incorrect Answers: " + incorrectCount + "<br>"
+        );
+        $("#score").append("<br>" + "Unanswered Questions: " + unAnsweredCount);
+        $("#startquiz").show();
+      }
     }
-   
-}
+  }
+  //Function to display the questions and choices (with radio buttons), creates new divs/forms for the radio buttons
 
-//function to create a Submit button for the radio button form
-  function createSubmit() {
+  function displayQuizContent() {
+    // $("#quiz-content").text("Time Remaining: " +number+ " seconds");
 
-    var newButton = $("<button/>");
-    
-       $(newButton).attr("class","btn btn-info btn-lg center-block");
-       $(newButton).attr("type","Submit");
-       $(newButton).attr("id","submitAnswers");
-       $(newButton).text("Submit Answers");
-       $("#question").append(newButton); 
-       $('#submitAnswers').click(function() {
-        event.preventDefault();
-        stop();
-    });
+    for (var i = 0; i < quizContent.length; i++) {
+      console.log(quizContent[i].question);
 
-       
+      console.log(quizContent[i].correctAnswer);
+
+      //Display question, then go through loop to display answers with radio buttons
+
+      $("#question").append("<br>" + quizContent[i].question + "<br>");
+
+      var newForm = $("<fieldset/>");
+
+      var cform = "choiceForm" + i;
+      var cname = "optradio" + i;
+
+      $(newForm).attr("id", cform);
+
+      $(newForm).attr("class", "form-group");
+
+      $(newForm).attr("name", cname);
+
+      $("#question").append(newForm);
+
+      for (var j = 0; j < 4; j++) {
+        var labelname = quizContent[i].choices[j];
+
+        var value = j;
+
+        var newId = "radio" + j;
+
+        var create = $(
+          '<input type="radio" name="' +
+            cname +
+            '"  id= "' +
+            newId +
+            '" value="' +
+            value +
+            '"><label class="radio-inline">' +
+            labelname +
+            "</label>"
+        );
+
+        $(newForm).append(create);
+
+        console.log("value: " + value);
+      }
+    }
+    //   $("#submitAnswers").show();
+    $(
+      "<input type='submit' value='Submit' id='submitAnswers' class='btn-lg center-block'/>"
+    ).appendTo(newForm);
   }
 
-        
+  // //  Start on click of "Start Quiz" button. Hide start quiz button, display submit answers button and call functions to display quiz content and start timer.
+  $("#startquiz").on("click", function() {
+    event.preventDefault();
 
-//Function to display the questions and choices (with radio buttons), creates new divs/forms for the radio buttons
-    
-      function displayQuizContent ()  {
+    $("#startquiz").hide();
+    $("#score").empty();
 
-        // $("#quiz-content").text("Time Remaining: " +number+ " seconds");
+    run();
 
+    displayQuizContent();
+  });
 
-          for (var i = 0; i < quizContent.length; i++) {
-
-              console.log(quizContent[i].question)
-
-              console.log(quizContent[i].correctAnswer)
-
-              //Display question, then go through loop to disoplay answers with radio buttons
-
-              $("#question").append("<br>"+quizContent[i].question+"<br>");
-
-                    var newForm = $("<form/>");
-
-                        var cform = "choiceForm" + i;
-
-                        $(newForm).attr("id", cform);
-
-                        $(newForm).attr("class", "form-group");
-
-                        $("#question").append(newForm);
-
-                        for (var j = 0; j < 4; j++) {
-
-                                var labelname = quizContent[i].choices[j];
-
-                                  var value = j;
-
-                                  var newId = "radio" + j;
+  $("#question").on("submit", function(e) {
+    e.preventDefault();
+    clearInterval(intervalId);
+    number = 30;
 
 
-                                    var create = $('<input type="radio" name="optradio" id= "'+newId+'" value="'+value+'"><label class="radio-inline">'+labelname+'</label>');
-                                      
-                                    $(newForm).append(create);
-                                 
-                                             console.log("value: " + value);                                                                              
-                        }  
-                        
-            }
-                                createSubmit();
-                                     
-}
 
-//Function to loop through the forms of radio buttons and compare answers that are checked to the correcrt answer - not working!!!
+      var checkedButtons = [];
+      var correctCount = 0;
+      var incorrectCount = 0;
+      var unAnsweredCount = 0;
+  //Store radio button values in an array
+  var responseVal = $("#choiceForm0 input:checked").val()
+  checkedButtons.push(responseVal)
+  var responseVal = $("#choiceForm1 input:checked").val()
+  checkedButtons.push(responseVal)
+  var responseVal = $("#choiceForm2 input:checked").val()
+  checkedButtons.push(responseVal)
+  var responseVal = $("#choiceForm3 input:checked").val()
+  checkedButtons.push(responseVal)
+  var responseVal = $("#choiceForm4 input:checked").val()
+  checkedButtons.push(responseVal)
+  var responseVal = $("#choiceForm5 input:checked").val()
+  checkedButtons.push(responseVal)
 
-//       scoreCheck();
+  console.log("Answers selected: " + checkedButtons)
+  console.log("Correct Answers: " + correctAnswers)
 
-// scoreCheck();
-//             function scoreCheck()      {
+  for (var k = 0; k < 6; k++) {
 
+  if (checkedButtons[k] === undefined){
+      unAnsweredCount++
+  }
+  else if (checkedButtons[k] == correctAnswers[k]){
+      correctCount++
+  }
+  else {
+      incorrectCount++
+  }
 
-                // for (var k = 0; k < quizContent.length; k++) {
+  }
 
+  $("#quiz-content").empty();
+  $("#status").empty();
+  $("#question").empty();
+  $("#submitAnswers").hide();
+  $("#score").html("Game Over!<br>");
+  $("#score").append("<br>"+"Score: "+Math.round((correctCount/quizContent.length)*100)+"%"+"<br>");
+  $("#score").append("<br>"+"Correct Answers: " + correctCount + "<br>");
+  $("#score").append("<br>"+"Incorrect Answers: " + incorrectCount + "<br>");
+  $("#score").append("<br>"+"Unanswered Questions: " + unAnsweredCount);
 
-                //     var userAnswer = ('#choiceForm'+k+"input[name=optradio]:checked") == true.valueOf();
+  $("#startquiz").show();
+  console.log("correct: " +correctCount+ "incorrect: " +incorrectCount+ "unanswered: " + unAnsweredCount)
 
-                //     (checkedButtons).push(userAnswer)
-
-                //     if (checkedButtons[k]===quizContent[k].correctAnswer){
-
-                //         correctCount++
-
-                        // console.log(correctCount)
-                        // console.log(userAnswer)
-                        // console.log("Question" +k+ checkedButtons)
-
-        
-                    
- 
-     
-               
-    //    }
-      
-     
-
-
-        });
-
-            
-                          
-
-                       
-                        
+  })
+});
